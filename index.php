fdedd1a076ebf2b90c44c5fbdcdaee3a6b497e05
clarissairene <164118797+clarissairene@users.noreply.github.com>
2024-11-29 11:09:05 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// Data dummy
+$nama = "Irene";
+$institusi = "Unika";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP Dummy Page</title>
+</head>
+<body>
+	<h1>Halo, saya <?= $nama; ?></h1>
+	<p><?= $institusi; ?></p>
+</body>
+</html>
